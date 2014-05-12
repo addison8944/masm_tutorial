b51807edf9c6e05be79c46e5e4eef864e0609dc3
THOMIAS Yannis <thomias.yannis@gmail.com>
2014-05-12 16:13:29 +0200

    bootloader

---
+++ bootloader/bootloader.asm
@@ -0,0 +1,44 @@
+.286
+.model TINY
+
+.code
+	cli
+	mov ax,cs               ; Setup segment registers
+	mov ds,ax               ; Make DS correct
+	mov es,ax               ; Make ES correct
+	mov ss,ax               ; Make SS correct
+	mov bp,7c00h
+	mov sp,7c00h            ; Setup a stack
+	sti
+
+	; clear screen
+	mov al, 02h
+	mov ah, 00h
+	int 10h
+
+	; show cursor
+	mov cx, 0007h
+	mov ah, 01h
+	int 10h
+
+	; print msg
+	mov si, offset msg
+putloop:
+	mov al,[si]
+	add si,1
+	cmp al,0
+	je finish
+	mov ah,0eh
+	mov bx,15
+	int 10h
+	jmp putloop
+finish:
+	hlt
+	jmp finish
+msg:
+	db "Hello Yannis OS!",13,10,0
+
+	org 510
+	signature dw 0AA55h
+
+END
